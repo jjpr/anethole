5628f3cf0505061f723d03b1880286b75b98daf1
Jonathan J. Prescott-Roy <jjprescottroy@gmail.com>
2016-01-22 15:58:59 -0500

    Remove reference to conda.  Added iPython magic to make mayavi release the kernel when it shows the figure.  Removed now-redundant mlab.show().  This gracefully pops up an X window on mlab.mesh(), and releases the kernel, and subsequent mlab.savefig() works.

--- start_notebook.sh
+++ start_notebook.sh
@@ -9,7 +9,7 @@ docker create -p 2222:22 -p 80:8888 -v ~/Documents/Kitematic/$CONTAINER_NAME/dat
 
 docker start $CONTAINER_NAME
 
-(sleep 20; open "http://$(docker-machine ip default)") &
+(sleep 10; open "http://$(docker-machine ip default)") &
 
 expect -c "spawn ssh -t -Y -p 2222 root@$(docker-machine ip default) jupyter notebook --no-browser --ip=* --notebook-dir=/data; expect \"assword:\"; send \"password\r\"; interact"
 
