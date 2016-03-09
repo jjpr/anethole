b3c24fefa59fec2b7d3ffafa0a2577acfeaf9a75
Jonathan J. Prescott-Roy <jjprescottroy@gmail.com>
2016-03-09 15:23:35 -0500

    Made solidify.py reusable.

--- docker/equation-to-object/solidify.py
+++ docker/equation-to-object/solidify.py
@@ -1,7 +1,13 @@
 import bpy
+import sys
 
-filepath_input = "/data/notebooks/wrapped_ruffle.obj"
-filepath_output = "/data/notebooks/wrapped_ruffle.stl"
+argv = sys.argv[sys.argv.index("--") + 1:]
+
+print(argv[0])
+
+filepath_input = argv[0]
+filepath_output = argv[1]
+thickness = float(argv[2])
 
 bpy.ops.import_scene.obj(filepath=filepath_input)
 
@@ -9,7 +15,7 @@ obj = bpy.data.objects['grp1']
 print(obj.name)
 
 obj.modifiers.new("solidified", type="SOLIDIFY")
-obj.modifiers["solidified"].thickness = 1.0
+obj.modifiers["solidified"].thickness = thickness
 obj.modifiers["solidified"].offset = 0.0
 
 bpy.context.scene.objects.active = obj
