c0bc21bb2b318e3c10da441e895c39a3c4785e06
Jonathan J. Prescott-Roy <jjprescottroy@gmail.com>
2016-06-27 13:47:21 -0400

    solidify_x3d.py will add thickness to an X3D file, preserving colormap.  Not yet tested.

---
+++ docker/equation-to-object/solidify_x3d.py
@@ -0,0 +1,24 @@
+import bpy
+import sys
+
+argv = sys.argv[sys.argv.index("--") + 1:]
+
+print(argv[0])
+
+filepath_input = argv[0]
+filepath_output = argv[1]
+thickness = float(argv[2])
+
+bpy.ops.import_scene.x3d(filepath=filepath_input)
+
+obj = bpy.data.objects['ShapeIndexedFaceSet']
+print(obj.name)
+
+obj.modifiers.new("solidified", type="SOLIDIFY")
+obj.modifiers["solidified"].thickness = thickness
+obj.modifiers["solidified"].offset = 0.0
+
+bpy.context.scene.objects.active = obj
+bpy.ops.object.modifier_apply(apply_as='DATA', modifier="solidified")
+
+bpy.ops.export_scene.x3d(filepath=filepath_output)
